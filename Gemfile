7578980d5a0416957fa569824c5aa29c1daa22f3
Geoffrey Litt <gklitt@gmail.com>
2012-07-23 04:00:06 +0100

    db changes for heroku

--- Gemfile
+++ Gemfile
@@ -7,7 +7,14 @@ gem 'rake'
 
 gem 'activerecord'
 gem 'sinatra-activerecord'
-gem 'sqlite3'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+end
 
 gem 'haml'
 gem 'psych' # Provides UTF-8 output
--- app.rb
+++ app.rb
@@ -10,12 +10,32 @@ end
 require './course'
 require './scrape'
 
+env = ENV["RACK_ENV"] || 'development'
+
+if env == 'development'
+
 set :database, 'sqlite://development.db'
 
   ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3',
     database: 'development.db'
 )
+
+else
+
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+
+end
 
 set :static, true
 
